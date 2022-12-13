cbb3d7fb724c5520960b7a1e3ac603df0d958961
zaid <zald0001@student.monash.edu>
2022-12-13 16:57:53 +0800

    use program in terminal with different arguments and hash algorithms

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,6 @@
 import hashlib
+import argparse
+from colorama import Fore
 
 
 def read_file(file_name):
@@ -21,15 +23,39 @@ def SHA1(arr):
     return res
 
 
-def hash_to_password(common_passwords, hashed_passwords, hash):
+def SHA256(arr):
+    res = []
+    for i in arr:
+        res.append(hashlib.sha256(i.encode()).hexdigest())
+    return res
+
+
+def MD5(arr):
+    res = []
+    for i in arr:
+        res.append(hashlib.md5(i.encode()).hexdigest())
+    return res
+
+
+def SHA512(arr):
+    res = []
+    for i in arr:
+        res.append(hashlib.sha512(i.encode()).hexdigest())
+    return res
+
+
+def hash_to_password(common_passwords, hashed_passwords, hash, verbose=False):
     for i in range(len(hashed_passwords)):
+        if verbose:
+            print("Trying password " + str(i+1) + ": " + common_passwords[i])
         if hashed_passwords[i] == hash:
             return common_passwords[i]
-    return "PASSWORD NOT IN DATABASE"
+    return Fore.RED + "PASSWORD NOT IN DATABASE" + Fore.RESET
 
 
-def crack_sha1_hash(hash, use_salts=False):
-    common_passwords = read_file("top-10000-passwords.txt")
+def crack(hash, file_name, use_salts=False, mode="SHA1", verbose=False):
+    common_passwords = read_file(file_name)
+    hashed_passwords = []
     if use_salts:
         # load known salts into a list
         salts = read_file("known-salts.txt")
@@ -48,17 +74,68 @@ def crack_sha1_hash(hash, use_salts=False):
                 new_passwords.append(password)
 
         # Calculate the SHA-1 hash of the password+salt
-        hashed_passwords = SHA1(passwords_with_salts)
-        return hash_to_password(new_passwords, hashed_passwords, hash)
+        if mode == "SHA1":
+            hashed_passwords = SHA1(passwords_with_salts)
+        elif mode == "SHA256":
+            hashed_passwords = SHA256(passwords_with_salts)
+        elif mode == "SHA512":
+            hashed_passwords = SHA512(passwords_with_salts)
+        elif mode == "MD5":
+            hashed_passwords = MD5(passwords_with_salts)
+        return Fore.GREEN + "Password found: " + hash_to_password(new_passwords, hashed_passwords, hash, verbose) + Fore.RESET
     else:
-        hashed_passwords = SHA1(common_passwords)
-        return hash_to_password(common_passwords, hashed_passwords, hash)
+        if mode == "SHA1":
+            hashed_passwords = SHA1(common_passwords)
+        elif mode == "SHA256":
+            hashed_passwords = SHA256(common_passwords)
+        elif mode == "SHA512":
+            hashed_passwords = SHA512(common_passwords)
+        elif mode == "MD5":
+            hashed_passwords = MD5(common_passwords)
+        return Fore.GREEN + "Password found: " + hash_to_password(common_passwords, hashed_passwords, hash, verbose) + Fore.RESET
 
 
 def main():
-    pw = crack_sha1_hash(
-        "53d8b3dc9d39f0184144674e310185e41a87ffd5", use_salts=True)
-    print(pw)
+    parser = argparse.ArgumentParser(description="Hash Cracker")
+
+    # add a positional argument for the path to the wordlist
+    parser.add_argument(
+        "wordlist", help="path to the wordlist to use for cracking the hash")
+
+    # add a positional argument for the hash to crack
+    parser.add_argument("hash", help="the hash to crack")
+
+    # add an optional argument for the cracking mode (default to "fast")
+    parser.add_argument("-m", "--mode", default="SHA1",
+                        choices=["SHA1", "SHA256", "SHA512", "MD5"], help="cracking algorithm to use")
+
+    # add an optional flag to enable verbose output
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="enable verbose output")
+
+    # add an optional flag to enable salted cracking
+    parser.add_argument("-s", "--salt", action="store_true",
+                        help="enable salted cracking")
+
+    # parse the arguments
+    args = parser.parse_args()
+
+    # extract the arguments
+    wordlist = args.wordlist
+    hash_to_crack = args.hash
+    mode = args.mode
+    verbose = args.verbose
+    use_salts = args.salt
+
+    result = crack(hash_to_crack, wordlist, use_salts=use_salts,
+                   mode=mode, verbose=verbose)
+
+    # print the arguments to the terminal
+    print(Fore.YELLOW + "Wordlist:", wordlist + Fore.RESET)
+    print(Fore.YELLOW + "Hash to crack:", hash_to_crack + Fore.RESET)
+    print(Fore.CYAN + "Mode:", mode + Fore.RESET)
+    print(Fore.CYAN + "Verbose:", str(verbose) + Fore.RESET)
+    print(result)
 
 
 if __name__ == '__main__':
--- test_module.py
+++ test_module.py
@@ -4,7 +4,7 @@ import password_cracker
 
 class UnitTests(unittest.TestCase):
     def test_hash_1(self):
-        actual = password_cracker.crack_sha1_hash(
+        actual = password_cracker.crack(
             "18c28604dd31094a8d69dae60f1bcd347f1afc5a")
         expected = "superman"
         self.assertEqual(
@@ -13,7 +13,7 @@ class UnitTests(unittest.TestCase):
         )
 
     def test_hash_2(self):
-        actual = password_cracker.crack_sha1_hash(
+        actual = password_cracker.crack(
             "5d70c3d101efd9cc0a69f4df2ddf33b21e641f6a")
         expected = "q1w2e3r4t5"
         self.assertEqual(
@@ -22,7 +22,7 @@ class UnitTests(unittest.TestCase):
         )
 
     def test_hash_3(self):
-        actual = password_cracker.crack_sha1_hash(
+        actual = password_cracker.crack(
             "b80abc2feeb1e37c66477b0824ac046f9e2e84a0")
         expected = "bubbles1"
         self.assertEqual(
@@ -31,7 +31,7 @@ class UnitTests(unittest.TestCase):
         )
 
     def test_hash_4(self):
-        actual = password_cracker.crack_sha1_hash(
+        actual = password_cracker.crack(
             "80540a46a2c1a0eae58d9868f01c32bdcec9a010")
         expected = "01071988"
         self.assertEqual(
@@ -40,7 +40,7 @@ class UnitTests(unittest.TestCase):
         )
 
     def test_hash_salted_1(self):
-        actual = password_cracker.crack_sha1_hash(
+        actual = password_cracker.crack(
             "53d8b3dc9d39f0184144674e310185e41a87ffd5", use_salts=True)
         expected = "superman"
         self.assertEqual(
@@ -49,7 +49,7 @@ class UnitTests(unittest.TestCase):
         )
 
     def test_hash_salted_2(self):
-        actual = password_cracker.crack_sha1_hash(
+        actual = password_cracker.crack(
             "da5a4e8cf89539e66097acd2f8af128acae2f8ae", use_salts=True)
         expected = "q1w2e3r4t5"
         self.assertEqual(
@@ -58,7 +58,7 @@ class UnitTests(unittest.TestCase):
         )
 
     def test_hash_salted_3(self):
-        actual = password_cracker.crack_sha1_hash(
+        actual = password_cracker.crack(
             "ea3f62d498e3b98557f9f9cd0d905028b3b019e1", use_salts=True)
         expected = "bubbles1"
         self.assertEqual(
@@ -67,7 +67,7 @@ class UnitTests(unittest.TestCase):
         )
 
     def test_hash_salted_4(self):
-        actual = password_cracker.crack_sha1_hash(
+        actual = password_cracker.crack(
             "05bbf26a28148f531cf57872df546961d1ed0861", use_salts=True)
         expected = "01071988"
         self.assertEqual(
@@ -76,7 +76,7 @@ class UnitTests(unittest.TestCase):
         )
 
     def test_not_in_database(self):
-        actual = password_cracker.crack_sha1_hash(
+        actual = password_cracker.crack(
             "03810a46a2c1a0eae58d9332f01c32bdcec9a01a")
         expected = "PASSWORD NOT IN DATABASE"
         self.assertEqual(
